c19eca1bebe55a30835f112d309895fa6ca42784
Asif Ishtiaq <ishtiaqasif03@gmail.com>
2021-07-28 20:44:52 +0600

    Project Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './Components/bs-navbar/bs-navbar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './Components/check-out/check-out.component';
+import { OrderSuccessComponent } from './Components/order-success/order-success.component';
+import { MyOrdersComponent } from './Components/my-orders/my-orders.component';
+import { AdminProductsComponent } from './Components/Admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './Components/Admin/admin-orders/admin-orders.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'check-out', component: CheckOutComponent },
+      { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'my/orders', component: MyOrdersComponent },
+      { path: 'login', component: LoginComponent },
+      
+      { path: 'admin/products', component: AdminProductsComponent },
+      { path: 'admin/orders', component: AdminOrdersComponent },
+    ]),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBg-0DTkXW_O5B2uZF0gxIUAKufjJhhFaI",
+    authDomain: "oshop-c6474.firebaseapp.com",
+    projectId: "oshop-c6474",
+    storageBucket: "oshop-c6474.appspot.com",
+    messagingSenderId: "1027235823754",
+    appId: "1:1027235823754:web:67ec888dbfa138268bad73",
+    measurementId: "G-Z0XHH6XRJT"
+  }
 };
 
 /*
